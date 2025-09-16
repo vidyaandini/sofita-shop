44eaebb6a04284c6523634a32775dfecf134eb96
vidyaandini <vidyapramudita07@gmail.com>
2025-09-16 13:24:26 +0700

    Tugas 3

--- sofita_shop/settings.py
+++ sofita_shop/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "vidya-pramudita-sofitashop.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "<url-deployment-pws-kamu>"
+    "https://vidya-pramudita-sofitashop.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
