cdb6242cbf790be54782c68d5782fe88231904f0
vidyaandini <vidyapramudita07@gmail.com>
2025-09-21 20:27:50 +0700

    Tugas 4 bismillah fix

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main, create_product, show_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user
+from main.views import show_main, create_product, show_product, show_xml, show_json, show_xml_by_id, show_json_by_id, register, login_user, logout_user
 
 app_name = 'main'
 
@@ -13,4 +13,5 @@ urlpatterns = [
     path('json/<str:product_id>/', show_json_by_id, name='show_json_by_id'),
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
